e49f25fc442119769f576e76b521e626ef252888
nathaly <nathalysalgado.f@gmail.com>
2017-03-06 15:45:41 -0500

    zoom effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,6 @@
-/*Carousel*/
-$('.hero-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    navText: [
-              "<img src='assets/icons/boton-izquierda.png' class=''></img>",
-              "<img src='assets/icons/boton-derecha.png' class=''></img>"
-            ],
-    
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+function autoResizeDiv()
+        {
+            document.getElementById('main').style.height = window.innerHeight +'px';
         }
-    }
-})
+        window.onresize = autoResizeDiv;
+        autoResizeDiv();
