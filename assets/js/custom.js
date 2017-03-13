fb3e031ed74a0d62987d27feb40b67052d5f980d
Nathaly <nathalysalgado.f@gmail.com>
2017-03-13 11:48:57 -0500

    responsive index

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+$('.slide-g').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    navText: [
+              "<img src='assets/icons/btn-izq.png' class=''></img>",
+              "<img src='assets/icons/btn-der.png' class=''></img>"
+            ],
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
